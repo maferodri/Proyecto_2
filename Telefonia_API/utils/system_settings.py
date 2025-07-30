2c425a0f2e45682c7e5ea7950d8bf0d772b436fa
maferodri <rodriguezfer673@gmail.com>
2025-07-29 18:59:16 -0600

    Delete Telefonia_API directory

--- Telefonia_API/utils/system_settings.py
+++
@@ -1,9 +0,0 @@
-from models.system_settings import SystemSetting
-from utils.mongodb import get_collection
-
-async def get_system_setting(key: str) -> SystemSetting:
-    collection = get_collection("System Settings")
-    doc = await collection.find_one({"key": key})
-    if not doc:
-        raise Exception(f"Setting '{key}' no encontrado")
-    return SystemSetting(**doc)

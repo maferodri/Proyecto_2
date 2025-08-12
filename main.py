d0d82ff7f2c1b3a89318fbe75825ce80e146804e
maferodri <rodriguezfer673@gmail.com>
2025-08-12 16:50:42 -0600

    fix: add middleware

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credential=True, 
+    allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"], 
 )

3ee1dfabf44f1d98e8b89d499a4b72cafc4b0e95
maferodri <rodriguezfer673@gmail.com>
2025-08-13 19:44:23 -0600

    fix: pipeline lookup 2

--- pipelines/inventory_pipelines.py
+++ pipelines/inventory_pipelines.py
@@ -1,79 +1,153 @@
+# pipelines/inventory_pipelines.py
 from bson import ObjectId
-
-def validate_inventory_type_pipeline(inventory_type_id: str) -> list:
-    return [
-        {"$match": {"_id": ObjectId(inventory_type_id), "active": True}},
-        {"$project": {"id": {"$toString": "$_id"}, "name": 1, "active": 1}}
-    ]
 
 def get_inventory_with_type_pipeline(inventory_id: str) -> list:
     return [
         {"$match": {"_id": ObjectId(inventory_id)}},
-        {"$addFields": {"id_inventory_type_obj": {"$toObjectId": "$id_inventory_type"}}},
-        {"$lookup": {
-            "from": "inventorytypes",
-            "localField": "id_inventory_type_obj",
-            "foreignField": "_id",
-            "as": "inventory_type"
-        }},
-        {"$unwind": "$inventory_type"},
-        {"$project": {
-            "_id": 0,
-            "id": {"$toString": "$_id"},
-            "id_inventory_type": {"$toString": "$id_inventory_type"},
-            "name": 1,
-            "creation_date": 1,
-            "active": 1,
-            "inventory_type_name": "$inventory_type.name"
-        }}
+        {
+            "$addFields": {
+                "id_inventory_type_obj": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        "$id_inventory_type",
+                        {"$toObjectId": "$id_inventory_type"},
+                    ]
+                }
+            }
+        },
+        {
+            "$lookup": {
+                "from": "inventorytypes",   # <- minúscula
+                "localField": "id_inventory_type_obj",
+                "foreignField": "_id",
+                "as": "inv_type"
+            }
+        },
+        {"$unwind": {"path": "$inv_type", "preserveNullAndEmptyArrays": True}},
+        {
+            "$project": {
+                "_id": 0,
+                "id": {"$toString": "$_id"},
+                "id_inventory_type": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        {"$toString": "$id_inventory_type"},
+                        "$id_inventory_type",
+                    ]
+                },
+                "name": 1,
+                "creation_date": 1,
+                "active": 1,
+                "inventory_type_name": {"$ifNull": ["$inv_type.name", "$inv_type.description"]},
+            }
+        }
     ]
 
-def get_all__with_types_pipeline(skip: int = 0, limit: int = 10) -> list:
+
+def get_inventories_by_type_pipeline(inventory_type_name: str, skip: int = 0, limit: int = 10) -> list:
     return [
-        {"$addFields": {"id_inventory_type_obj": {"$toObjectId": "$id_inventory_type"}}},
-        {"$lookup": {
-            "from": "inventorytypes",
-            "localField": "id_inventory_type_obj",
-            "foreignField": "_id",
-            "as": "inventory_type"
-        }},
-        {"$unwind": "$inventory_type"},
-        {"$match": {"inventory_type.active": True}},  # opcional: sólo tipos activos
-        {"$project": {
-            "_id": 0,
-            "id": {"$toString": "$_id"},
-            "id_inventory_type": {"$toString": "$id_inventory_type"},
-            "name": 1,
-            "creation_date": 1,
-            "active": 1,
-            "inventory_type_name": "$inventory_type.name"
-        }},
+        {
+            "$addFields": {
+                "id_inventory_type_obj": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        "$id_inventory_type",
+                        {"$toObjectId": "$id_inventory_type"},
+                    ]
+                }
+            }
+        },
+        {
+            "$lookup": {
+                "from": "inventorytypes",   # <- minúscula
+                "localField": "id_inventory_type_obj",
+                "foreignField": "_id",
+                "as": "inv_type"
+            }
+        },
+        {"$unwind": {"path": "$inv_type", "preserveNullAndEmptyArrays": False}},
+        {
+            "$match": {
+                "inv_type.name": {"$regex": f"^{inventory_type_name}$", "$options": "i"},
+                "active": True
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "id": {"$toString": "$_id"},
+                "id_inventory_type": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        {"$toString": "$id_inventory_type"},
+                        "$id_inventory_type",
+                    ]
+                },
+                "name": 1,
+                "creation_date": 1,
+                "active": 1,
+                "inventory_type_name": {"$ifNull": ["$inv_type.name", "$inv_type.description"]},
+            }
+        },
         {"$skip": int(skip)},
-        {"$limit": int(limit)}
+        {"$limit": int(limit)},
     ]
 
-def get_inventories_by_type_name_pipeline(type_name: str, skip: int = 0, limit: int = 10) -> list:
+
+def get_all_inventories_with_types_pipeline(skip: int = 0, limit: int = 10) -> list:
     return [
-        {"$addFields": {"id_inventory_type_obj": {"$toObjectId": "$id_inventory_type"}}},
-        {"$lookup": {
-            "from": "inventorytypes",
-            "localField": "id_inventory_type_obj",
-            "foreignField": "_id",
-            "as": "inventory_type"
-        }},
-        {"$unwind": "$inventory_type"},
-        {"$match": {
-            "inventory_type.name": {"$regex": f"^{type_name}$", "$options": "i"},
-            "active": True
-        }},
-        {"$project": {
-            "_id": 0,
-            "id": {"$toString": "$_id"},
-            "id_inventory_type": {"$toString": "$id_inventory_type"},
-            "name": 1,
-            "creation_date": 1,
-            "active": 1
-        }},
+        {
+            "$addFields": {
+                "id_inventory_type_obj": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        "$id_inventory_type",
+                        {"$toObjectId": "$id_inventory_type"},
+                    ]
+                }
+            }
+        },
+        {
+            "$lookup": {
+                "from": "inventorytypes",   # <- minúscula
+                "localField": "id_inventory_type_obj",
+                "foreignField": "_id",
+                "as": "inv_type"
+            }
+        },
+        {"$unwind": {"path": "$inv_type", "preserveNullAndEmptyArrays": True}},
+        {"$match": {"inv_type.active": True}},
+        {
+            "$project": {
+                "_id": 0,
+                "id": {"$toString": "$_id"},
+                "id_inventory_type": {
+                    "$cond": [
+                        {"$eq": [{"$type": "$id_inventory_type"}, "objectId"]},
+                        {"$toString": "$id_inventory_type"},
+                        "$id_inventory_type",
+                    ]
+                },
+                "name": 1,
+                "creation_date": 1,
+                "active": 1,
+                "inventory_type_name": {"$ifNull": ["$inv_type.name", "$inv_type.description"]},
+            }
+        },
         {"$skip": int(skip)},
-        {"$limit": int(limit)}
+        {"$limit": int(limit)},
+    ]
+
+
+def validate_inventory_type_pipeline(inventory_type_id: str) -> list:
+    return [
+        {"$match": {"_id": ObjectId(inventory_type_id), "active": True}},
+        {
+            "$project": {
+                "_id": 0,
+                "id": {"$toString": "$_id"},
+                "name": 1,
+                "active": 1
+            }
+        }
     ]

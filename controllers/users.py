719d92053fad0675977a90e99ccba0d7a1ec657c
maferodri <rodriguezfer673@gmail.com>
2025-08-14 18:23:38 -0600

    contraseña arreglo

--- controllers/users.py
+++ controllers/users.py
@@ -42,28 +42,39 @@ initialize_firebase()
 
 users_coll = get_collection("Users") 
 
-async def create_user(user : User)  -> User: 
-    user_record = {} 
-    
-    try: 
-        user_record = firebase_auth.create_user( 
-            email=user.email, 
-            password=user.password 
+async def create_user( user: User ) -> User:
+
+    user_record = {}
+    try:
+        user_record = firebase_auth.create_user(
+            email=user.email
+            , password=user.password
         )
     except Exception as e:
-        logger.warning(e)    
-        raise HTTPException( 
-             status_code=400 
-            ,detail="Error in registrating user in FireBase"
+        logger.warning( e )
+        raise HTTPException(
+            status_code=400
+            , detail="Error al registrar usuario en firebase"
         )
-    
+
     try:
-        user_dict = user.model_dump(exclude={"id", "password"}) 
-        inserted = users_coll.insert_one( user_dict) 
-        user.id = str(inserted.inserted_id) 
-        return user 
-    except Exception as e: 
-        firebase_auth.delete_user( user_record.uid ) 
+        coll = get_collection("users")
+
+        new_user = User(
+            name=user.name
+            , lastname=user.lastname
+            , email=user.email
+            , password=user.password
+        )
+
+        user_dict = new_user.model_dump(exclude={"id", "password"})
+        inserted = coll.insert_one(user_dict)
+        new_user.id = str(inserted.inserted_id)
+        new_user.password = "*********"  # Mask the password in the response
+        return new_user
+
+    except Exception as e:
+        firebase_auth.delete_user(user_record.uid)
         logger.error(f"Error creating user: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
     
